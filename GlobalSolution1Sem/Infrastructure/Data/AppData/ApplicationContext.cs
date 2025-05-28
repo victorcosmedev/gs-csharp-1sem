86d949602be230d5fdd9df6d0cc1e487f7ae51ec
victorcosmedev <victorcosme.profissional@gmail.com>
2025-05-28 19:55:55 -0300

    Feat: Implementação do ApplicationContext e as credenciais de banco

---
+++ GlobalSolution1Sem/Infrastructure/Data/AppData/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using GlobalSolution1Sem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GlobalSolution1Sem.Infrastructure.Data.AppData
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        public DbSet<EnderecoEntity> Endereco { get; set; }
+        public DbSet<UsuarioEntity> Usuario { get; set; }
+        public DbSet<PostEntity> Post { get; set; }
+    }
+}
