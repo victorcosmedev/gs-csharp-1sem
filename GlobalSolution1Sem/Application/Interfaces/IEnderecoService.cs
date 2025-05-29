1246032e9bf372296ee9b3e14c8be773dd78a577
victorcosmedev <victorcosme.profissional@gmail.com>
2025-05-29 20:24:32 -0300

    feat: Implementada a lógica do EnderecoService.

--- GlobalSolution1Sem/Application/Interfaces/IEnderecoService.cs
+++ GlobalSolution1Sem/Application/Interfaces/IEnderecoService.cs
@@ -5,8 +5,8 @@ namespace GlobalSolution1Sem.Application.Interfaces
     public interface IEnderecoService
     {
         Task<EnderecoDto> CadastrarEnderecoAsync(EnderecoDto endereco);
-        Task<EnderecoDto> AtualizarEnderecoAsync(EnderecoDto endereco);
-        Task<bool> RemoverEnderecoAsync(string cep, string numero);
+        Task<EnderecoDto> AtualizarEnderecoAsync(int id, EnderecoDto endereco);
+        Task<bool> RemoverEnderecoAsync(int id);
         Task<EnderecoDto?> ObterEnderecoPorCepNumeroAsync(string cep, string numero);
         Task<EnderecoDto?> BuscarEnderecoPorIdAsync(int id);
         Task<IEnumerable<EnderecoDto>> ListarTodosEnderecosAsync();
--- GlobalSolution1Sem/Application/Services/EnderecoService.cs
+++ GlobalSolution1Sem/Application/Services/EnderecoService.cs
@@ -9,41 +9,94 @@ namespace GlobalSolution1Sem.Application.Services
     public class EnderecoService : IEnderecoService
     {
         private readonly IEnderecoRepository _enderecoRepository;
+        private readonly IUsuarioRepository _usuarioRepository;
         private readonly IMapper _mapper;
-        public EnderecoService(IEnderecoRepository enderecoRepository, IMapper mapper)
+
+        public EnderecoService(IEnderecoRepository enderecoRepository, IUsuarioRepository usuarioRepository, IMapper mapper)
         {
             _enderecoRepository = enderecoRepository;
+            _usuarioRepository = usuarioRepository;
             _mapper = mapper;
         }
 
-        public Task<EnderecoDto> AtualizarEnderecoAsync(EnderecoDto endereco)
+        public async Task<EnderecoDto> AtualizarEnderecoAsync(int id, EnderecoDto endereco)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var entity = _mapper.Map<EnderecoEntity>(endereco);
+                var usuario = await AtribuirEValidarUsuarioAsync(endereco.UsuarioId, entity);
+                entity.Usuario = usuario;
+
+                entity = await _enderecoRepository.UpdateAsync(id, entity);
+
+                return _mapper.Map<EnderecoDto>(entity);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
-        public Task<EnderecoDto?> BuscarEnderecoPorIdAsync(int id)
+        public async Task<EnderecoDto?> BuscarEnderecoPorIdAsync(int id)
         {
-            throw new NotImplementedException();
+            var entity = await _enderecoRepository.GetByIdAsync(id);
+            var dto = _mapper.Map<EnderecoDto>(entity);
+            return dto;
         }
 
-        public Task<EnderecoDto> CadastrarEnderecoAsync(EnderecoDto endereco)
+        public async Task<EnderecoDto> CadastrarEnderecoAsync(EnderecoDto endereco)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var entity = _mapper.Map<EnderecoEntity>(endereco);
+                var usuario = await AtribuirEValidarUsuarioAsync(endereco.UsuarioId, entity);
+                entity.Usuario = usuario;
+
+                entity = await _enderecoRepository.AddAsync(entity);
+                return _mapper.Map<EnderecoDto>(entity);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
         }
 
-        public Task<IEnumerable<EnderecoDto>> ListarTodosEnderecosAsync()
+        public async Task<IEnumerable<EnderecoDto>> ListarTodosEnderecosAsync()
         {
-            throw new NotImplementedException();
+            var entities = await _enderecoRepository.GetAllAsync();
+            var dtos = entities.Select(e => _mapper.Map<EnderecoDto>(e)).ToList();
+            return dtos;
         }
 
-        public Task<EnderecoDto?> ObterEnderecoPorCepNumeroAsync(string cep, string numero)
+        public async Task<EnderecoDto?> ObterEnderecoPorCepNumeroAsync(string cep, string numero)
         {
-            throw new NotImplementedException();
+            var entity = await _enderecoRepository.GetByCepAndNumeroAsync(cep, numero);
+            var dto = _mapper.Map<EnderecoDto?> (entity);
+            return dto;
         }
 
-        public Task<bool> RemoverEnderecoAsync(string cep, string numero)
+        public async Task<bool> RemoverEnderecoAsync(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return await _enderecoRepository.DeleteAsync(id);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private async Task<UsuarioEntity> AtribuirEValidarUsuarioAsync(int usuarioId, EnderecoEntity entity)
+        {
+            var usuario = await _usuarioRepository.GetById(usuarioId);
+            if (usuario == null)
+                throw new Exception("Usuário não existe");
+
+            if (usuario.EnderecoId != entity.Id)
+                throw new Exception("Usuário já está cadastrado num endereço");
+
+            return usuario;
         }
     }
 }
