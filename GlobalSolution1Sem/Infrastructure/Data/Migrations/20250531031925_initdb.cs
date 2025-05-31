72388f3887e0e904792bc46ee30017105bb5ff65
victorcosmedev <victorcosme.profissional@gmail.com>
2025-05-31 00:20:03 -0300

    chore: Feitas as migrations.

---
+++ GlobalSolution1Sem/Infrastructure/Data/Migrations/20250531031925_initdb.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GlobalSolution1Sem.Migrations
+{
+    /// <inheritdoc />
+    public partial class initdb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tb_cl_usuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Nome = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Cpf = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    EnderecoId = table.Column<int>(type: "NUMBER(10)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_cl_usuario", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tb_cl_endereco",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Logradouro = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Numero = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Cep = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Complemento = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    UsuarioId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_cl_endereco", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_tb_cl_endereco_tb_cl_usuario_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "tb_cl_usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tb_cl_post",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Titulo = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Descricao = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    UsuarioId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_cl_post", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_tb_cl_post_tb_cl_usuario_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "tb_cl_usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_cl_endereco_UsuarioId",
+                table: "tb_cl_endereco",
+                column: "UsuarioId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_cl_post_UsuarioId",
+                table: "tb_cl_post",
+                column: "UsuarioId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tb_cl_endereco");
+
+            migrationBuilder.DropTable(
+                name: "tb_cl_post");
+
+            migrationBuilder.DropTable(
+                name: "tb_cl_usuario");
+        }
+    }
+}
